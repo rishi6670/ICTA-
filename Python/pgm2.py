b8e9435adbef12c3f146780c029470a7f23d7d6e
rishi6670 <48901332+rishi6670@users.noreply.github.com>
2019-04-02 12:20:45 +0530

    getting input and print output

---
+++ Python/pgm2.py
@@ -0,0 +1,14 @@
+name=raw_input("Enter u r name")
+print name
+
+#age=raw_input("Rnter u r age:")
+#print age
+
+#mark=raw_input("Rnter u r mark")
+#print mark
+
+age=int(input("Rnter u r age:"))
+print age
+
+mark=float(input("Rnter u r mark"))
+print mark
