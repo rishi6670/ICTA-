1fc73036c0762020a7ab8e0f93d0c51b6ce2c002
rishi6670 <48901332+rishi6670@users.noreply.github.com>
2019-04-02 14:49:48 +0530

    if statement

---
+++ Python/large2.py
@@ -0,0 +1,14 @@
+a=int(input("Enter first number :"))
+b=int(input("Enter second number :"))
+c=int(input("Enter third number :"))
+#if a>b:
+#	print a
+#else:
+#	print b
+
+if a>b & a>c:
+	print a
+elif b>c:
+	print b
+else:
+	print c
