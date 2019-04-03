fa56f5c6687947de95431186097c512fcab915e1
rishi6670 <48901332+rishi6670@users.noreply.github.com>
2019-04-03 15:12:50 +0530

    datatype list and its operation

---
+++ Python/list.py
@@ -0,0 +1,41 @@
+#a = [1,"hello",3.5,[2,"welcome",4.5]]
+
+#print a
+
+#print a[1]
+
+#for i in a:
+#    print i
+
+#a[0]=2
+#print a[0]
+
+#a[3][1]="hai"
+#print a[3][1]
+
+#a=[1,2,3]
+#b=a+[1]
+#print b
+#c=b*3
+#print c
+
+#a=[1,2,3]
+#a.append(4) #to add an element in last position 
+#print a
+
+#a=[1,2,3]
+#b=[4,5,6]
+#a.extend(b) #to add second list to the first list
+#print a
+
+#a=[0,1,2,3]
+#a.insert(1,2) #insert a value into list (index,value)
+#print a
+
+#a=[0,1,2,3]
+#a.pop()  #to delete last element from list
+#print a
+
+a=[0,1,2,3]
+a.pop(0) # to delete an element from list pop(index)
+print a
