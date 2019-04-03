cd5a1048505ee57004b81ce549e8ba238fbf9b7f
rishi6670 <48901332+rishi6670@users.noreply.github.com>
2019-04-03 10:12:04 +0530

    scope of variable

---
+++ Python/scope.py
@@ -0,0 +1,12 @@
+#def test():
+#	print "inside test is",x
+#x=50
+#test()
+#print "value of x is",x
+
+def test():
+	y=20
+	print "value of x is",x,"value of y is",y
+x=50
+test()
+print "value of x is",x,"value of yis",y
